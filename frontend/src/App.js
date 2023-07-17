74be02f68a9b29b7c4593ad7bb5827cf467ccf7d
danutdotrar <morarasu.danut@gmail.com>
2023-07-17 16:21:59 +0300

    Add Product and HomeScreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
     return (
         <>
             <Header />
-            <Container>
-                <main className="py-3">
-                    <h1>Welcome to E-Commerce app</h1>
-                </main>
-            </Container>
+            <main className="py-3">
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
             <Footer />
         </>
     );
