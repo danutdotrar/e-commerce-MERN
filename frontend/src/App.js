4897038428f4b46e425d9a8f4416264bed34de87
danutdotrar <morarasu.danut@gmail.com>
2023-07-17 15:54:47 +0300

    Add bootstrap header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
     return (
         <>
-            <h1>Welcome to E-Commerce app</h1>
+            <Header />
+            <Container>
+                <main className="py-3">
+                    <h1>Welcome to E-Commerce app</h1>
+                </main>
+            </Container>
+            <Footer />
         </>
     );
 };
